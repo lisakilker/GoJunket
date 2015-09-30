0e508206e3279aba2fd23fb129ee7d54c0d2e53d
Lisa Kilker <lisakilker@gmail.com>
2015-09-30 20:29:02 +0200

    updated

--- GoJunket/app/models/user.rb
+++ GoJunket/app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 	devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "150x150#" }, :default_url => "/images/:style/missing.png"
+	has_attached_file :avatar, :styles => { :medium => "250x250>", :thumb => "150x150#" }, :default_url => "/images/:style/missing.png"
 	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
 	validates :name, presence: true
@@ -19,7 +19,7 @@ class User < ActiveRecord::Base
 	enum education: [:High_School, :Course_Certification, :College, :Grad_School, :PhD, :high_school, :course_certification, :college, :grad_school, :phd]
 
 	 has_and_belongs_to_many :interests
- accepts_nested_attributes_for :interests, :update_only => true, :reject_if => :all_blank
+ accepts_nested_attributes_for :interests, :allow_destroy => true, :reject_if => :all_blank
  
  has_and_belongs_to_many :languages
  accepts_nested_attributes_for :languages, :allow_destroy => true, :reject_if => :all_blank
