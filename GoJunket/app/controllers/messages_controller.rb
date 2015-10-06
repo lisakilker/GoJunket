dc97579b3cad848c370be6c1c23348656c11859a
Lisa Kilker <lisakilker@gmail.com>
2015-10-06 22:38:08 +0200

    working version

--- GoJunket/app/controllers/messages_controller.rb
+++ GoJunket/app/controllers/messages_controller.rb
@@ -3,17 +3,25 @@ class MessagesController < ApplicationController
   before_action :set_recipient, only: [:new, :create]
 
 	def new
-		@message = Message.new
-		@message = current_user.sent_messages.new
+		@user = @recipient
+		@message = @recipient.messages.new
 	end
 
 	def create
-		@message = current_user.message.new message_params
+		new_params = message_params
+		new_params[:times] = new_params[:times].to_i
+		new_params[:activities] = new_params[:activities].to_i
+		@message = Message.new(new_params)
+		@message.sender_id = current_user.id
 		@message.recipient_id = @recipient.id
-		if @message.save
-    		redirect_to :users
-		end
-	end
+		 if @message.save
+      		flash[:success] = "Message sent!"
+      		redirect_to users_path
+    	else
+      		flash[:failure] = "Oops, message did not send.  Please try again."
+      		redirect_to root_path
+    	end
+    end
 
 	def index
 		@messages = current_user.messages
@@ -30,7 +38,7 @@ class MessagesController < ApplicationController
 	private
 
 	def message_params
-		params.require(:message).permit(:content, :recipient_id, :sender_id)
+		params.require(:message).permit(:content, :times, :activities)
    	end
 
 	def set_recipient
--- GoJunket/app/models/message.rb
+++ GoJunket/app/models/message.rb
@@ -7,4 +7,8 @@ class Message < ActiveRecord::Base
 	belongs_to :recipient, class_name: "User", foreign_key: "recipient_id"
 	belongs_to :sender, class_name: "User", foreign_key: "sender_id"
 	scope :unread, -> { where read: false }
+
+	enum activities: [:"Beach", :"Bike Ride", :"Coffee", :"Club", :"Couple's Date", :"Dinner", :"Drinks", :"Gym", :"Kid's Play Date", :"Lunch", :"Travel", :"Other"]
+
+	enum times: [:"Now", :"1:00 am", :"2:00 am", :"3:00 am", :"4:00 am" , :"5:00 am", :"6:00 am", :"7:00 am", :"8:00 am", :"9:00 am", :"10:00 am", :"11:00 am", :"Noon", :"1:00 pm", :"2:00 pm", :"3:00 pm", :"4:00 pm", :"5:00 pm", :"6:00 pm", :"7:00 pm", :"8:00 pm" , :"9:00 pm", :"10:00 pm", :"11:00 pm", :"Midnight"]
 end
